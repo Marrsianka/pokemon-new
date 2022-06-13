ed3640e65cfb09b3f155edba61da9de94098d55a
Marrsianka <marsianka@marsianka.us>
2022-06-13 18:04:38 +0300

    add pokemon's list
    test

--- script.js
+++ script.js
@@ -11,10 +11,21 @@
         let pokemonsColumn = document.createElement('div');
             let pokemonsItemCard = document.createElement('div');
                 let cardRow = document.createElement('div');
-                    let cardItemTitle = document.createElement('div');
+                    let cardItemTitle = document.createElement('div');                        
+                        let titleRow = document.createElement('div');
+                            let titleItemName = document.createElement('div');
+                            let titleItemRank = document.createElement('div');
                     let cardItemImage = document.createElement('div');
+                        let imgPokemon = document.createElement('img');
                     let cardItemFeatures = document.createElement('div');
 
+
+
+        titleItemName.textContent = database[i].name;
+        titleItemRank.textContent = '0';
+        imgPokemon.src = database[i].image;
+        imgPokemon.alt = database[i].name;
+        imgPokemon.title = database[i].name;
 
 
 
@@ -24,6 +35,11 @@
                 cardRow.classList.add('card__row');
                     cardItemTitle.classList.add('card__item');
                     cardItemTitle.classList.add('title');
+                        titleRow.classList.add('title__row');
+                        titleItemName.classList.add('title__item');
+                        titleItemName.classList.add('name');
+                        titleItemRank.classList.add('title__item');
+                        titleItemRank.classList.add('rank');
                     cardItemImage.classList.add('card__item');
                     cardItemImage.classList.add('image');
                     cardItemFeatures.classList.add('card__item');
@@ -37,7 +53,11 @@
             pokemonsColumn.appendChild(pokemonsItemCard);
                 pokemonsItemCard.appendChild(cardRow);
                     cardRow.appendChild(cardItemTitle);
+                        cardItemTitle.appendChild(titleRow);
+                        titleRow.appendChild(titleItemName);
+                        titleRow.appendChild(titleItemRank);
                     cardRow.appendChild(cardItemImage);
+                        cardItemImage.appendChild(imgPokemon);
                     cardRow.appendChild(cardItemFeatures);
     }
 })();
